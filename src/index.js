d7e740334368be959a9bfb05441013f5e8205a9a
newbie <newbie@newbies-MacBook-Pro.local>
2021-05-18 18:41:03 +0300

    add babel and create simple server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hey!');
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Booklab backend');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
